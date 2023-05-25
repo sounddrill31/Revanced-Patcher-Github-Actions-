9f547524bd9c8d34c16fc124b0324da48de2855a
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-05-25 17:12:03 +0530

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -6,8 +6,8 @@
 
 #output apk at ~/output
 
-java -jar ~/repo/*.jar \
- -a ~/input/*.apk \
- -o ~/output/revanced.apk \
- -b ~/patches/*.jar \
+java -jar /home/runner/repo/*.jar \
+ -a /home/runner/input/*.apk \
+ -o /home/runner/output/revanced.apk \
+ -b /home/runner/patches/*.jar \
  -e vanced-microg-support
